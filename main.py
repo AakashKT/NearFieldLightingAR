ee6f10dd5c6a4905f9d044eeaed12ff92b6199b1
AakashKT <aakash.kt@research.iiit.ac.in>
2020-11-09 23:25:20 +0530

    Envmap lit virtual object insertion

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-import os, sys, math, random
+import os, sys, math, random, argparse
 import torch, cv2, trimesh
+
+import numpy as np
+
+import envmap_sh
+import obj_sh
 
 def detect_marker(img_rgb, img_depth):
     # Detect where to place the object in the image.
     # We may have to physically place a marker in the scene
     # Return x, y coordinates, specifing the pixel
-    pass
+    if 'img1' in img_path or 'img2' in img_path:
+        return [352, 352]
+    elif 'img3' in img_path:
+        return [690, 188]
 
-def compute_virtual_obj_sh(virtual_obj):
+def compute_virtual_obj_sh(obj_file, cam_pos, cam_lookat):
     # Assume a simple material model for the object. For starters, make a uniform material (textures can be added later).
     # Compute SH coefs using MC integration. If uniform material, only 1 set of SH coefs. for the object.
     # This is potentially a precomputation, so doesnt matter if its computationally expensive.
-    pass
+    return obj_sh.compute_sh(obj_file, cam_pos, cam_lookat)
 
 def compute_envmap_sh(img_rgb, img_depth):
-    # Use the network from the following paper
-    # "Inverse Rendering for Complex Indoor Scenes: Shape, Spatially-Varying Lighting and SVBRDF from a Single Image"
-    pass
+    # Downscale image (4032x3024 --> 63x47)
+    img_rgb = cv2.resize(img_rgb, (63, 47))
+
+    return envmap_sh.compute_sh(img_rgb)
 
 def detect_near_field(img_rgb, img_depth):
     # Use a simple YCbCr conversion, and threshold on the luma (Y) value.
@@ -32,39 +41,96 @@ def compose(img_rgb, img_depth, virtual_obj_sh, env_map_sh, near_field_sh):
     # Depth aware compositing. Needs more thought, not very sure what to do.
     pass
 
+def load_images(img_path, img_depth_path):
+    img_rgb = cv2.cvtColor( cv2.imread(img_path), cv2.COLOR_BGR2RGB )
+    img_depth = cv2.cvtColor( cv2.imread(img_depth_path), cv2.COLOR_BGR2RGB )
+
+    # Normalize 0 to 1
+    img_rgb = img_rgb / 255.0
+    img_depth = img_depth / 255.0
+
+    return img_rgb, img_depth
+
+def compute_camera(img_path, img_rgb, img_depth):
+    if 'img1' in img_path or 'img2' in img_path:
+        cam_pos = [3, 3, 3]
+        cam_lookat = [0, 0, 0]
+
+        return cam_pos, cam_lookat
+    elif 'img3' in img_path:
+        cam_pos = [-0.5, 2, 4.3]
+        cam_lookat = [0, 0, 0]
+
+        return cam_pos, cam_lookat
+
+
 if __name__ == '__main__':
     # Process one frame, can easily be extended to a video
 
     # -- Process command line args -- #
-    # Assuming image_name.png is RGB and image_name_depth.png is depth 
-    img_path = sys.argv[1]
-    img_depth_path = img_path.replace('.png', '') + '_depth.png'
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--image', type=str, help='')
+    parser.add_argument('--obj_sh', type=str, default='NULL', help='')
+    parser.add_argument('--obj_file', type=str, default='NULL', help='')
 
-    # Load obj model using trimesh.
-    obj_path = sys.argv[2] 
-    virtual_obj = trimesh.load(file_obj=obj_path, use_embree=True) # Embree because ray intersection tests might be required
+    args = parser.parse_args()
 
-    img_rgb = cv2.cvtColor( cv2.imread(img_path), cv2.COLOR_BGR2RGB )
-    img_depth = cv2.cvtColor( cv2.imread(img_depth_path), cv2.COLOR_BGR2RGB )
+    # Assuming image_name.JPG is RGB and image_name_depth.JPG is depth 
+    img_path = args.image
+    img_depth_path = img_path.replace('.JPG', '_depth.JPG')
+
+    # Load RGB and Depth image
+    img_rgb, img_depth = load_images(img_path, img_depth_path)
+
+    # Determine camera parameters
+    cam_pos, cam_lookat = compute_camera(img_path, img_rgb, img_depth)
 
     # Detect loaction in the scene, in which to place the object
     obj_marker = detect_marker(img_rgb, img_depth)
 
     # SH coefs for virtual obj
-    virtual_obj_sh = compute_virtual_obj_sh(virtual_obj)
+    if args.obj_sh == 'NULL':
+        virtual_obj_sh = compute_virtual_obj_sh(args.obj_file, cam_pos, cam_lookat)
+
+        npy_file = args.obj_file.replace('.obj', '_sh.npy')
+        np.save(npy_file, virtual_obj_sh)
+    else:
+        virtual_obj_sh = np.load(args.obj_sh)
 
     # Environment lighting SH
     env_map_sh = compute_envmap_sh(img_rgb, img_depth)
+    env_map_sh = np.concatenate(env_map_sh, axis=0)
+    env_map_sh = np.array([[env_map_sh]])
+    env_map_sh = np.tile(env_map_sh, (200, 200, 1))
+
+    obj_rendered = np.zeros((200, 200, 3), dtype=np.float)
+    obj_rendered_ = env_map_sh * virtual_obj_sh
+    for i in range(0, 9):
+        obj_rendered[:, :, 0] += obj_rendered_[:, :, i*3]
+        obj_rendered[:, :, 1] += obj_rendered_[:, :, i*3+1]
+        obj_rendered[:, :, 2] += obj_rendered_[:, :, i*3+2]
+    
+    mask = img_rgb.copy()
+    mask[:, :, :] = 0
+    mask[obj_marker[0]-100:obj_marker[0]+100, obj_marker[1]-100:obj_marker[1]+100, :] = obj_rendered
+    img_rgb[mask > 0.001] = 0
+
+    obj_rendered = obj_rendered**(1.0/2.4)
+    
+    img_rgb[obj_marker[0]-100:obj_marker[0]+100, obj_marker[1]-100:obj_marker[1]+100, :] += obj_rendered
+    final = np.clip(img_rgb, 0, 1) * 255.0
+    final = cv2.cvtColor(final.astype(np.uint8), cv2.COLOR_RGB2BGR)
+    cv2.imwrite('render_re.png', final)
 
     # Near-field lighting
-    near_field_lights = detect_near_field(img_rgb, img_depth)
-    near_field_sh = []
-    for light in near_field_lights:
-        sh = compute_near_field_sh(img_rgb, img_depth, light)
-        near_field_sh.append(sh)
+    # near_field_lights = detect_near_field(img_rgb, img_depth)
+    # near_field_sh = []
+    # for light in near_field_lights:
+    #     sh = compute_near_field_sh(img_rgb, img_depth, light)
+    #     near_field_sh.append(sh)
     
-    # Combine both types of lighting
-    final_augmented = compose(img_rgb, img_depth, virtual_obj_sh, env_map_sh, near_field_sh)
+    # # Combine both types of lighting
+    # final_augmented = compose(img_rgb, img_depth, virtual_obj_sh, env_map_sh, near_field_sh)
 
-    output_path = img_path.replace('.png', '') + '_augmented.png'
-    cv2.imsave(output_path, final_augmented)
+    # output_path = img_path.replace('.png', '') + '_augmented.png'
+    # cv2.imsave(output_path, final_augmented)
---
+++ obj_sh.py
@@ -0,0 +1,174 @@
+import torch, os, sys, cv2, json, argparse, random, glob, struct, math, time
+import torch.nn as nn
+from torch.nn import init
+import functools
+import torch.optim as optim
+
+from torch.utils.data import Dataset, DataLoader
+from torch.nn import functional as func
+from PIL import Image
+
+import scipy.ndimage as ndimage
+import torchvision.transforms as transforms
+import numpy as np 
+import os.path as osp
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+import pyexr
+
+import enoki as ek
+import mitsuba
+mitsuba.set_variant('gpu_rgb')
+
+from mitsuba.core import Vector3f, Float, Float32, Float64, Thread, xml, Spectrum, depolarize, RayDifferential3f, Frame3f, warp, Bitmap, Struct
+from mitsuba.core import math as m_math
+from mitsuba.core.xml import load_string, load_file
+from mitsuba.render import BSDF, Emitter, BSDFContext, BSDFSample3f, SurfaceInteraction3f, ImageBlock, register_integrator, register_bsdf, MonteCarloIntegrator, SamplingIntegrator, has_flag, BSDFFlags, DirectionSample3f
+
+import numpy as np
+
+def sph_dir(theta, phi):
+    """ Map spherical to Euclidean coordinates """
+    st, ct = ek.sincos(theta)
+    sp, cp = ek.sincos(phi)
+    return Vector3f(cp*st, sp*st, ct)
+
+def sph_convert(v):
+    x2 = ek.pow(v.x, 2)
+    y2 = ek.pow(v.y, 2)
+    z2 = ek.pow(v.z, 2)
+
+    r = ek.sqrt(x2+y2+z2)
+    phi = ek.atan2(v.y, v.x)
+    theta = ek.atan2(ek.sqrt(x2+y2), v.z)
+
+    return r, theta, phi
+
+def y_0_0(colors, theta, phi):
+    K = ek.sqrt( 1.0/(4*ek.pi) )
+    return K * colors
+
+def y_1_n1(colors, theta, phi):
+    K = ek.sqrt( 3.0/(4*ek.pi) )
+    return K * ek.sin(phi) * -ek.sin(theta) * colors
+
+def y_1_0(colors, theta, phi):
+    K = ek.sqrt( 3.0/(4*ek.pi) )
+    return K * ek.cos(theta) * colors
+
+def y_1_p1(colors, theta, phi):
+    K = ek.sqrt( 3.0/(4*ek.pi) )
+    return K * ek.cos(phi) * -ek.sin(theta) * colors
+
+def y_2_n2(colors, theta, phi):
+    K = ek.sqrt( 15.0/(4*ek.pi) )
+    return K * ek.sin(phi) * ek.cos(phi) * ek.pow( ek.sin(theta), 2 ) * colors
+
+def y_2_n1(colors, theta, phi):
+    K = ek.sqrt( 15.0/(4*ek.pi) )
+    return K * ek.sin(phi) * ek.sin(theta) * -ek.cos(theta) * colors
+
+def y_2_0(colors, theta, phi):
+    K = ek.sqrt( 5.0/(16*ek.pi) )
+    return K * ( 3*ek.pow( ek.cos(theta), 2 )-1 ) * colors
+
+def y_2_p1(colors, theta, phi):
+    K = ek.sqrt( 15.0/(4*ek.pi) )
+    return K * ek.cos(phi) * ek.sin(theta) * -ek.cos(theta) * colors
+
+def y_2_p2(colors, theta, phi):
+    K = ek.sqrt( 15.0/(16*ek.pi) )
+    return K * ( ek.pow( ek.cos(phi), 2 )-ek.pow( ek.sin(phi), 2 ) ) * ek.pow( ek.sin(theta), 2 ) * colors
+
+class AuxIntegrator(SamplingIntegrator):
+    def __init__(self, props):
+        SamplingIntegrator.__init__(self, props)
+
+    def sample(self, scene, sampler, ray, medium=None, active=True):
+        result = Vector3f(0.0)
+        si = scene.ray_intersect(ray, active)
+        active = si.is_valid() & active
+
+        ctx = BSDFContext()
+        bsdf = si.bsdf(ray)
+
+        # bs, bsdf_val = BSDF.sample_vec(bsdf, ctx, si, sampler.next_1d(active), sampler.next_2d(active), active)
+        # pdf = bs.pdf
+        # wo = si.to_world(bs.wo)
+
+        ds, emitter_val = scene.sample_emitter_direction(si, sampler.next_2d(active), True, active)
+        pdf = ds.pdf
+        active_e = active & ek.neq(ds.pdf, 0.0)
+        wo = ds.d
+        bsdf_val = BSDF.eval_vec(bsdf, ctx, si, wo, active_e)
+        emitter_val[emitter_val > 1.0] = 1.0
+        bsdf_val *= emitter_val
+        
+        # _, wi_theta, wi_phi = sph_convert(si.to_world(si.wi))
+        _, wo_theta, wo_phi = sph_convert(wo)
+
+        y_0_0_ = y_0_0(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+        
+        y_1_n1_ = y_1_n1(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+        y_1_0_ = y_1_0(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+        y_1_p1_ = y_1_p1(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+
+        y_2_n2_ = y_2_n2(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+        y_2_n1_ = y_2_n1(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+        y_2_0_ = y_2_0(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+        y_2_p1_ = y_2_p1(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+        y_2_p2_ = y_2_p2(bsdf_val, wo_theta, wo_phi) / ek.select(pdf > 0, pdf, Float(0.01))
+
+        result.x = si.uv.x
+        result.y = si.uv.y
+
+        return result, si.is_valid(), [ Float(y_0_0_[0]), Float(y_0_0_[1]), Float(y_0_0_[2]),\
+                                        Float(y_1_n1_[0]), Float(y_1_n1_[1]), Float(y_1_n1_[2]),\
+                                        Float(y_1_0_[0]), Float(y_1_0_[1]), Float(y_1_0_[2]),\
+                                        Float(y_1_p1_[0]), Float(y_1_p1_[1]), Float(y_1_p1_[2]),\
+                                        Float(y_2_n2_[0]), Float(y_2_n2_[1]), Float(y_2_n2_[2]),\
+                                        Float(y_2_n1_[0]), Float(y_2_n1_[1]), Float(y_2_n1_[2]),\
+                                        Float(y_2_0_[0]), Float(y_2_0_[1]), Float(y_2_0_[2]),\
+                                        Float(y_2_p1_[0]), Float(y_2_p1_[1]), Float(y_2_p1_[2]),\
+                                        Float(y_2_p2_[0]), Float(y_2_p2_[1]), Float(y_2_p2_[2]) ]
+
+    def aov_names(self):
+        names = []
+        for i in range(0, 9):
+            for c in ['r', 'g', 'b']:
+                names.append('sh_%s_%d' % (c, i))
+        
+        return names
+
+    def to_string(self):
+        return "AuxIntegrator[]"
+
+def compute_sh(obj_file, cam_pos, cam_lookat):
+    register_integrator('auxintegrator', lambda props: AuxIntegrator(props))
+
+    scene_template_file = './scene_template.xml'
+    Thread.thread().file_resolver().append(os.path.dirname(scene_template_file))
+
+    scene = load_file(scene_template_file, integrator='auxintegrator', fov="40", tx=cam_lookat[0], ty=cam_lookat[1], tz=cam_lookat[2], \
+                        ox=cam_pos[0], oy=cam_pos[1], oz=cam_pos[2], ux="0", uy="1", uz="0", spp="60", \
+                        width=200, height=200, obj_file=obj_file)
+    scene.integrator().render(scene, scene.sensors()[0])
+    film = scene.sensors()[0].film()
+    film.set_destination_file('./render_output.exr')
+    film.develop()
+
+    sh_channels_list = []
+    for i in range(0, 9):
+        for c in ['r', 'g', 'b']:
+            sh_channels_list.append('sh_%s_%d' % (c, i))
+
+    f_sh = np.zeros((200, 200, 27), dtype=np.float)
+    exrfile = pyexr.open('render_output.exr')
+
+    for i, channel in enumerate(sh_channels_list):
+        ch = exrfile.get(channel)
+        f_sh[:, :, i:i+1] += ch
+    
+    return f_sh
